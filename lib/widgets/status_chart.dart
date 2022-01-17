9f4328f6fc36ce79e579abe26b7991e93108c0f2
Ryota-nino <ryota.swimmer@icloud.com>
2022-01-17 16:07:57 +0900

    ステータス表示グラフを修正

--- lib/widgets/status_chart.dart
+++ lib/widgets/status_chart.dart
@@ -1,13 +1,15 @@
-import 'package:fl_chart/fl_chart.dart';
+// import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_charts/multi_charts.dart';
+import 'package:app/importer.dart';
 
-const gridColor = Color(0xff68739f);
-const titleColor = Color(0xff8c95db);
-const fashionColor = Color(0xffe15665);
-const artColor = Color(0xff63e7e5);
-const boxingColor = Color(0xff83dea7);
-const entertainmentColor = Colors.white70;
-const offRoadColor = Color(0xFFFFF59D);
+// const gridColor = Color(0xff68739f);
+// const titleColor = Color(0xff8c95db);
+// const fashionColor = Color(0xffe15665);
+// const artColor = Color(0xff63e7e5);
+// const boxingColor = Color(0xff83dea7);
+// const entertainmentColor = Colors.white70;
+// const offRoadColor = Color(0xFFFFF59D);
 
 class StatusRadarChart extends StatefulWidget {
   const StatusRadarChart({Key? key}) : super(key: key);
@@ -16,112 +18,72 @@ class StatusRadarChart extends StatefulWidget {
   _StatusRadarChartState createState() => _StatusRadarChartState();
 }
 
+var size = SizeConfig();
+
 class _StatusRadarChartState extends State<StatusRadarChart> {
   int selectedDataSetIndex = -1;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(height: 4),
-          AspectRatio(
-            aspectRatio: 1.3,
-            child: RadarChart(
-              RadarChartData(
-                radarTouchData: RadarTouchData(
-                    touchCallback: (FlTouchEvent event, response) {
-                  if (!event.isInterestedForInteractions) {
-                    setState(() {
-                      selectedDataSetIndex = -1;
-                    });
-                    return;
-                  }
-                  setState(() {
-                    selectedDataSetIndex =
-                        response?.touchedSpot?.touchedDataSetIndex ?? -1;
-                  });
-                }),
-                dataSets: showingDataSets(),
-                radarBackgroundColor: Colors.transparent,
-                borderData: FlBorderData(show: false),
-                radarBorderData: const BorderSide(color: Colors.transparent),
-                titlePositionPercentageOffset: 0.2,
-                titleTextStyle:
-                    const TextStyle(color: titleColor, fontSize: 14),
-                getTitle: (index) {
-                  switch (index) {
-                    case 0:
-                      return 'タンパク質'; // 上
-                    case 4:
-                      return '糖質'; // 右上
-                    case 2:
-                      return 'ミネラル'; // 右下
-                    case 3:
-                      return 'ビタミン'; // 左下
-                    case 1:
-                      return '脂質'; // 左上
-                    default:
-                      return '';
-                  }
-                },
-                tickCount: 1,
-                ticksTextStyle:
-                    const TextStyle(color: Colors.transparent, fontSize: 10),
-                tickBorderData: const BorderSide(color: Colors.transparent),
-                // gridBorderData: const BorderSide(color: gridColor, width: 2),
-              ),
-              swapAnimationDuration: const Duration(milliseconds: 400),
-            ),
+    size.init(context);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: size.deviceHeight * 0.4,
+          //Radar Chart
+          child: RadarChart(
+            values: [1, 2, 4, 7, 9],
+            labels: ["", "", "", "", ""],
+            maxValue: 50,
+            fillColor: Colors.blue,
+            chartRadiusFactor: 0.7,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
-  List<RadarDataSet> showingDataSets() {
-    return rawDataSets().asMap().entries.map((entry) {
-      var index = entry.key;
-      var rawDataSet = entry.value;
+  // List<RadarDataSet> showingDataSets() {
+  //   return rawDataSets().asMap().entries.map((entry) {
+  //     var index = entry.key;
+  //     var rawDataSet = entry.value;
 
-      final isSelected = index == selectedDataSetIndex
-          ? true
-          : selectedDataSetIndex == -1
-              ? true
-              : false;
+  //     final isSelected = index == selectedDataSetIndex
+  //         ? true
+  //         : selectedDataSetIndex == -1
+  //             ? true
+  //             : false;
 
-      return RadarDataSet(
-        fillColor: isSelected
-            ? rawDataSet.color.withOpacity(0.2)
-            : rawDataSet.color.withOpacity(0.05),
-        borderColor:
-            isSelected ? rawDataSet.color : rawDataSet.color.withOpacity(0.25),
-        entryRadius: isSelected ? 3 : 2,
-        dataEntries:
-            rawDataSet.values.map((e) => RadarEntry(value: e)).toList(),
-        borderWidth: isSelected ? 2.3 : 2,
-      );
-    }).toList();
-  }
+  //     return RadarDataSet(
+  //       fillColor: isSelected
+  //           ? rawDataSet.color.withOpacity(0.2)
+  //           : rawDataSet.color.withOpacity(0.05),
+  //       borderColor:
+  //           isSelected ? rawDataSet.color : rawDataSet.color.withOpacity(0.25),
+  //       entryRadius: isSelected ? 3 : 2,
+  //       dataEntries:
+  //           rawDataSet.values.map((e) => RadarEntry(value: e)).toList(),
+  //       borderWidth: isSelected ? 2.3 : 2,
+  //     );
+  //   }).toList();
+  // }
 
-  List<RawDataSet> rawDataSets() {
-    return [
-      RawDataSet(
-        title: 'nutrients',
-        color: fashionColor,
-        values: [
-          300,
-          230,
-          250,
-          180,
-          210,
-        ],
-      ),
-    ];
-  }
+  // List<RawDataSet> rawDataSets() {
+  //   return [
+  //     RawDataSet(
+  //       title: 'nutrients',
+  //       color: fashionColor,
+  //       values: [
+  //         300,
+  //         230,
+  //         250,
+  //         180,
+  //         210,
+  //       ],
+  //     ),
+  //   ];
+  // }
 }
 
 class RawDataSet {
