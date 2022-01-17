428becefb966024c9fc33a408360fd69f8af50e6
Ryota-nino <ryota.swimmer@icloud.com>
2022-01-17 16:08:08 +0900

    ライブラリ追加

--- lib/importer.dart
+++ lib/importer.dart
@@ -1,5 +1,6 @@
 // 共通
 export 'package:flutter/material.dart';
+export 'package:flutter_svg/flutter_svg.dart';
 
 // ホーム
 export 'widgets/avatar_button.dart';
@@ -11,11 +12,12 @@ export 'screens/battle_screen.dart';
 
 // 成長させる
 export 'screens/growth_screen.dart';
+export 'package:app/size_config.dart';
 
 // 称号一覧
 export 'screens/trophy_screen.dart';
 
 // ステータス
-export 'package:fl_chart/fl_chart.dart';
+// export 'package:fl_chart/fl_chart.dart';
 export 'package:app/widgets/dressup_button.dart';
 export 'dart:math';
